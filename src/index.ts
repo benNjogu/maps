13a2eba9c36208963b4ddffe679258b09b030f61
benNjogu <kamandemachafi@gmail.com>
2023-03-30 16:44:14 +0300

    Company class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { User } from './user/User';
+import { Company } from './company/Company';
 
 const user = new User();
 
 console.log(user);
+
+const company = new Company();
+
+console.log(company);
+
